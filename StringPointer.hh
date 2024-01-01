af971414f513bcb6541c5a024d9a6b32ca02cacf
iluzin <isluzin@gmail.com>
2024-01-01 21:18:31 +0300

    Create StringPointer.hh

---
+++ StringPointer.hh
@@ -0,0 +1,26 @@
+#ifndef __STRINGPOINTER_HH__
+#define __STRINGPOINTER_HH__
+
+class StringPointer {
+    std::string *pointer_;
+    static std::string empty_;
+    
+public:
+    std::string * operator->() {
+        return (std::string *)*this;
+    }
+    
+    operator std::string *() {
+        return pointer_ ? pointer_ : &empty_;
+    }
+    
+    StringPointer(std::string *Pointer):
+        pointer_(Pointer) {
+    }
+    
+    ~StringPointer() {}
+};
+
+std::string StringPointer::empty_;
+
+#endif
